f1e8af27730e79eb32094a90cf9877526b5247c6
Sashintha Tennakoon <sashitnha2001@gmail.com>
2021-04-09 15:02:28 -0400

    added outside HUD

---
+++ Assets/__Scripts/OutsideHUD.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OutsideHUD : MonoBehaviour
+{
+    public GameObject soulCounter;
+    string coins = GlobalVarStore.Coins.ToString();
+
+    void Start()
+    {
+        Text text = soulCounter.GetComponent<Text>();
+        text.text = "Souls: " + coins;
+    }
+
+}
