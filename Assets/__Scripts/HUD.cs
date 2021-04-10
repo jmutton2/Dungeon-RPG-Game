22cc27de78e6f37f4240d0af54f385d0b7645a3a
Sashintha Tennakoon <stennak@uwo.ca>
2021-04-09 21:45:42 -0400

    updated HUD

--- Assets/__Scripts/HUD.cs
+++ Assets/__Scripts/HUD.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class OutsideHUD : MonoBehaviour
+public class HUD : MonoBehaviour
 {
     public GameObject soulCounter;
     string coins = GlobalVarStore.Coins.ToString();
 
-    void Start()
+
+    void Update()
     {
         Text text = soulCounter.GetComponent<Text>();
         text.text = "Souls: " + coins;
