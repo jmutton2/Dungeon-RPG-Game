ae8e903bc4d84536b1d22b3486d14e5109a6ec36
jmutton2 <jmutton2@uwo.ca>
2021-04-14 03:32:31 -0400

    ending screen win screen

---
+++ Assets/__Scripts/WinScreen.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class WinScreen : MonoBehaviour
+{
+    public Text changingText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Gets the players final score
+        changingText.text = GlobalVarStore.Score.ToString();
+    }
+
+    public void Restart()
+    {
+        //Sets the scene to the main menu
+        SceneManager.LoadScene("MainMenu");
+    }
+}
